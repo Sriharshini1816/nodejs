412d8b21bcaa6d8dcd13d473edf9b78216e0f530
Sriharshini1816 <ponnamsriharshini@gmail.com>
2025-09-19 23:07:34 +0530

    Update users.js

--- users.js
+++ users.js
@@ -1,7 +1,7 @@
 // routes/users.js
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../User');
 const fetch = require('node-fetch');
 
 // get user (simple)
@@ -47,3 +47,4 @@ router.get('/:id/recommendations', async (req, res) => {
 });
 
 module.exports = router;
+
